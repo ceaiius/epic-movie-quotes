a225ab2a322e8a77304e1dcc9933889d0c98c9a7
ceaiius <n_mamaladze@cu.edu.ge>
2022-11-07 18:45:31 +0400

    feat: add quotes

---
+++ src/views/HomeView/NewsFeed/Dialogs/QuoteDialog.vue
@@ -0,0 +1,145 @@
+
+  <template>
+  <div
+    class="
+      flex flex-col
+      bg-[#11101A]
+      md:w-[900px] md:h-fit
+      pb-12
+      lg:ml-48 lg:mb-64
+      w-screen
+      h-screen
+      md:rounded-xl
+      rounded-none
+    "
+  >
+    <div class="flex flex-col items-center mt-6">
+      <h2 class="text-white">Write new quote</h2>
+      <hr class="w-full border-[#efefef4d] mt-6" />
+    </div>
+    <div class="flex justify-center">
+      <Form
+        class="mt-10 w-3/4"
+        enctype="multipart/form-data"
+        @submit="handleSubmit"
+      >
+        <div class="flex flex-col gap-6">
+          <div class="relative">
+            <InputTextArea
+              placeholder="New Quote"
+              name="name_en"
+              language="Eng"
+              rules="required|min:3"
+            />
+          </div>
+          <div class="relative">
+            <InputTextArea
+              placeholder="ახალი ციტატა"
+              language="ქარ"
+              name="name_ka"
+              rules="required|min:3|geo"
+            />
+          </div>
+          <div class="relative">
+            <InputField name="thumbnail" type="file" rules="required|min:3" />
+          </div>
+
+          <div class="relative">
+            <div>
+              <img
+                class="absolute top-5 left-4"
+                src="/images/camera.svg"
+                alt=""
+              />
+            </div>
+            <Field name="field">
+              <select
+                id="countries"
+                v-model="selectValue"
+                class="
+                  bg-black
+                  text-white
+                  rounded-lg
+                  block
+                  w-full
+                  p-2.5
+                  pl-16
+                  h-20
+                "
+              >
+                <option disabled value>Choose movie</option>
+                <option v-for="item in options" :key="item.id" :value="item.id">
+                  {{ i18n.global.locale == "En" ? item.name.en : item.name.ka }}
+                </option>
+              </select>
+            </Field>
+          </div>
+
+          <button
+            type="submit"
+            class="
+              text-white
+              bg-default_red
+              w-full
+              h-10
+              font-medium
+              rounded-md
+              text-sm
+              mt-6
+            "
+          >
+            Post
+          </button>
+        </div>
+      </Form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import InputTextArea from "../Form/InputTextArea.vue";
+import InputField from "../Form/InputField.vue";
+import axios from "@/config/axios/index.js";
+import { Form } from "vee-validate";
+import { i18n } from "../../../../i18n";
+import { onMounted, ref } from "vue";
+// eslint-disable-next-line no-unused-vars
+const emit = defineEmits(["updateQuotes", "closePopup"]);
+const url = import.meta.env.VITE_API_BASE_URL + "movies";
+const url_quotes = import.meta.env.VITE_API_BASE_URL + "quotes";
+const selectValue = ref("");
+const options = ref([]);
+
+onMounted(() => {
+  getMovies();
+});
+const getMovies = () => {
+  axios.get(url).then((res) => {
+    options.value = res.data;
+  });
+};
+const header = {
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+};
+const handleSubmit = (values) => {
+  axios
+    .post(
+      url_quotes,
+      {
+        name_en: values.name_en,
+        name_ka: values.name_ka,
+        thumbnail: values.thumbnail,
+        movie_id: selectValue.value,
+      },
+      header
+    )
+    .then((res) => {
+      if (res.status === 200) {
+        emit("updateQuotes");
+        emit("closePopup");
+      }
+    });
+};
+</script>
--- src/views/HomeView/NewsFeed/UserPost.vue
+++ src/views/HomeView/NewsFeed/UserPost.vue
@@ -17,9 +17,20 @@
         >
           <div class="flex pl-8 md:pl-4 gap-4 cursor-pointer">
             <img src="/images/pencil.svg" alt="" />
-            <h2 class="text-white">{{ $t("NewsFeed.write_new_quote") }}</h2>
+            <h2 class="text-white" @click="openQuote = true">
+              {{ $t("NewsFeed.write_new_quote") }}
+            </h2>
           </div>
         </div>
+
+        <teleport to="body">
+          <dialog-modal v-if="openQuote" @close="openQuote = false">
+            <QuoteDialog
+              @update-quotes="getQuotes"
+              @close-popup="openQuote = false"
+            />
+          </dialog-modal>
+        </teleport>
 
         <div class="h-[52px] lg:flex hidden relative ml-6">
           <div class="flex items-center">
@@ -33,27 +44,47 @@
           </div>
         </div>
       </div>
-      <div class="w-auto flex flex-col bg-[#11101A] mt-6 rounded-xl md:mb-12">
-        <div class="flex items-center gap-2 m-6 text-white w-12 h-12">
-          <img src="/images/static.png" alt="" />
-          <h2 class="whitespace-nowrap">Maia Nakashidze</h2>
-        </div>
-        <div class="m-6">
-          <h2 class="text-white">
-            "Follow your dream." movie - <span>Billy Eliot</span> (2000)
-          </h2>
-          <img class="md:max-w-3xl mt-2" src="/images/movie-image.png" alt="" />
-          <div class="flex gap-6 text-white m-6">
-            <div class="flex gap-2 cursor-pointer">
-              2 <img src="/images/comments.svg" alt="" />
-            </div>
-            <div class="flex gap-2 cursor-pointer">
-              10 <img src="/images/likes.svg" alt="" />
-            </div>
+      <div
+        v-for="item in data"
+        :key="item.id"
+        class="w-auto flex flex-col bg-[#11101A] mt-6 rounded-xl md:mb-12"
+      >
+        <div>
+          <div class="flex items-center gap-2 m-6 text-white w-12 h-12">
+            <img src="/images/static.png" alt="" />
+            <h2 class="whitespace-nowrap">{{ item.author.username }}</h2>
           </div>
-          <hr class="border-[#efefef4d]" />
+          <div class="m-6">
+            <h2 class="text-white">
+              "{{ i18n.global.locale == "En" ? item.name.en : item.name.ka }}" -
+              <span>{{
+                i18n.global.locale == "En"
+                  ? item.movies.name.en
+                  : item.movies.name.ka
+              }}</span>
+              {{ item.movies.year }}
+            </h2>
+            <div class="flex justify-center mt-2">
+              <img
+                class="md:max-w-3xl mt-2"
+                :src="url_thumbnail + item.thumbnail"
+                alt=""
+              />
+            </div>
+
+            <div class="flex gap-6 text-white m-6">
+              <div class="flex gap-2 cursor-pointer">
+                2 <img src="/images/comments.svg" alt="" />
+              </div>
+              <div class="flex gap-2 cursor-pointer">
+                10 <img src="/images/likes.svg" alt="" />
+              </div>
+            </div>
+            <hr class="border-[#efefef4d]" />
+          </div>
         </div>
-        <div class="m-6 flex gap-6 text-white">
+
+        <div class="m-6 flex gap-6 relative text-white">
           <div>
             <img src="/images/static.png" class="md:w-12 md:h-12" alt="" />
           </div>
@@ -95,12 +126,28 @@
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { i18n } from "../../../i18n";
+import axios from "@/config/axios/index.js";
+import DialogModal from "../../../components/DialogModal.vue";
+import QuoteDialog from "./Dialogs/QuoteDialog.vue";
+const url_thumbnail = import.meta.env.VITE_API_STORAGE_URL;
+const url = import.meta.env.VITE_API_BASE_URL + "quotes";
+const openQuote = ref(false);
 const commentLocale = computed(
   () => i18n.global.messages[i18n.global.locale].NewsFeed.write_comment
 );
 const searchLocale = computed(
   () => i18n.global.messages[i18n.global.locale].NewsFeed.search
 );
+const data = ref([]);
+onMounted(() => {
+  getQuotes();
+});
+const getQuotes = () => {
+  axios.get(url).then((res) => {
+    data.value = res.data;
+    console.log(res.data);
+  });
+};
 </script>
